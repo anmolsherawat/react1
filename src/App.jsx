d19f0d7568335c15756f5faa4e2826d6d176be11
AnmolSherawat <anmolsherawat07@gmail.com>
2025-04-01 09:48:31 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App =()=> {
+  return (
+    <div>
+      <h1>Score Board</h1>
+      <h1>Score : 0</h1>
+      <h1>Balls: 0</h1>
+    </div>
+  )
+}
+
+export default App
