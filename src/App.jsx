889c1fbd5d9e97bc9e5cca6d78d7df1dabcf899d
AnmolSherawat <anmolsherawat07@gmail.com>
2025-04-01 10:20:06 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const App =()=> {
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [balls, setBalls] = useState(0);
+
+  const addRuns = (runs) => {
+    setScore(score + runs);
+    setBalls(balls + 1);
+  };
+
   return (
     <div>
       <h1>Score Board</h1>
-      <h1>Score : 0</h1>
-      <h1>Balls: 0</h1>
-      <button>0 run</button>
-      
+      <h2>Score: {score}</h2>
+      <h2>Balls: {balls}</h2>
+      <button onClick={() => addRuns(0)}>0 run</button>
+      <button onClick={() => addRuns(1)}>1 run</button>
+      <button onClick={() => addRuns(2)}>2 runs</button>
+      <button onClick={() => addRuns(3)}>3 runs</button>
+      <button onClick={() => addRuns(4)}>4 runs</button>
+      <button onClick={() => addRuns(5)}>5 runs</button>
+      <button onClick={() => addRuns(6)}>6 runs</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
